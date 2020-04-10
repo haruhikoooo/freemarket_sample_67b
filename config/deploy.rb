ed4b2b29fd240cc2de1c406071bd9063f96fcb3d
tetsuji1000 <kontetsu0206@gmail.com>
2020-04-10 12:11:49 +0900

    AWS

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-set :linked_files, %w{ config/credentials.yml }
+set :linked_files, %w{ config/secret.yml }
 
 
 # after 'deploy:publishing', 'deploy:restart'
