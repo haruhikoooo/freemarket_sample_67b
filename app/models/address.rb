2a9692d03a1e302c808c8e3e33f6b4902fb074f1
takacpa <t.ura0315@gmail.com>
2020-04-17 21:01:30 +0900

    set_address-model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,13 @@
 class Address < ApplicationRecord
+  validates :destination_family_name, presence: true
+  validates :destination_first_name, presence: true
+  validates :destination_furigana_family, presence: true
+  validates :destination_furigana_first, presence: true
+  validates :zipcode, presence: true
+  validates :prefecture_id, presence: true
+  validates :city, presence: true
+  validates :house_number, presence: true
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :furigana_family, presence: true
+  validates :furigana_first, presence: true
+  validates :birthday, presence: true
+  has_one :payment, dependent: :destroy
+  has_one :address, dependent: :destroy
+  has_many :goods, dependent: :destroy
+  has_many :likes
+  has_many :goods_likes, through: :likes, source: :good, dependent: :destroy
+  has_many :comments
+  has_many :goods_comments, through: :comments, source: :good, dependent: :destroy
+  has_many :trannsactions
+  has_many :goods_transactions, through: :transactions, source: :good, dependent: :destroy
 end
