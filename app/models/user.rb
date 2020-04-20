5368ef8fd23645bbcb757783aa5d5c4ae477965c
takacpa <t.ura0315@gmail.com>
2020-04-20 20:06:40 +0900

    set user_ragistration_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,13 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :password, presence: true
   validates :family_name, presence: true
+  validates :family_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name, presence: true
+  validates :first_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :furigana_family, presence: true
+  validates :furigana_family,  format: { with: /\A[ぁ-んァ-ヶー－]+\z/}
   validates :furigana_first, presence: true
+  validates :furigana_first,  format: { with: /\A[ぁ-んァ-ヶー－]+\z/}
   validates :birthday, presence: true
   has_one :payment, dependent: :destroy
   has_one :address, dependent: :destroy
