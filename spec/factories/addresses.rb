f5469f9b63d375ba6f687d9b121060224f42e597
takacpa <t.ura0315@gmail.com>
2020-04-20 20:43:47 +0900

    set factorybot

---
+++ spec/factories/addresses.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+
+  factory :address do
+    destination_family_name        {"山田"}
+    destination_first_name         {"太郎"}
+    destination_furigana_family    {"ヤマダ"}
+    destination_furigana_first     {"タロウ"}
+    prefecture_id                  {1}
+    city                           {"名古屋市"}
+    house_number                   {"4番地"}
+    apartment_name                 {"フリマ名古屋"}
+    tel                            {"012-345-6789"}
+  end
+
+end
