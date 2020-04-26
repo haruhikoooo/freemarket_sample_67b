916a98c230d0eabd5af2ed22be023c52ab0495b8
haruhikoooo <haruhiko.ooka@gmail.com>
2020-04-26 13:41:18 +0900

    create logout.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
   
   devise_scope :user do
     get 'users/address' => 'users/registrations#new_address'
     post 'users/address' => 'users/registrations#create_address'
     get 'users/complete' => 'users/registrations#complete'
+    get 'users/logout' => 'users/sessions#logout'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -16,7 +18,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     resources :payments, only: [:index, :new, :create, :destroy]
   end
-  
+
 end
 
 
