e9d31bd4c1b34e01b08b0010edebfd2904aea476
tetsuji1000 <kontetsu0206@gmail.com>
2020-04-26 17:34:17 +0900

    goodsテーブル2つあるので一つコメントアウト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_082640) do
+# ActiveRecord::Schema.define(version: 2020_04_25_082640) do
 ActiveRecord::Schema.define(version: 2020_04_17_095238) do
 ActiveRecord::Schema.define(version: 2020_04_24_110205) do
 
