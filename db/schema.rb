f66ebd79cb34eadfb85c434952b08b85761c0f52
haruhikoooo <haruhiko.ooka@gmail.com>
2020-04-28 21:09:31 +0900

    add column to goods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_114629) do
+ActiveRecord::Schema.define(version: 2020_04_28_115829) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "destination_family_name", null: false
@@ -32,6 +32,9 @@ ActiveRecord::Schema.define(version: 2020_04_28_114629) do
   create_table "goods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.integer "transaction_status_id", null: false
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
