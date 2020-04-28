0fb0ce74f9bc6d26790068e5428c60f4399460d3
takacpa <t.ura0315@gmail.com>
2020-04-28 20:43:27 +0900

    add header_category-nav_function

--- app/assets/javascripts/goods.js
+++ app/assets/javascripts/goods.js
@@ -9,7 +9,8 @@ $(function(){
       clearTimeout(closetimer)
       opentimer = setTimeout(function(){
         categoryList.removeClass('hidden');
-      }, 500)
+      }, 100)
+      
     },
     'mouseout': function(){
       clearTimeout(opentimer)
@@ -17,46 +18,74 @@ $(function(){
         categoryList.addClass('hidden');
         $('.second-category-list').not('hidden').addClass('hidden');
         $('.third-category-list').not('hidden').addClass('hidden');
-      }, 400)
+      }, 100)
     }
   })
   var opentimerSecond
   var closetimerSecond
-  $('.category-list__item').on({
+  $('.category-list__item--link').on({
     'mouseover': function(){
-      clearTimeout(closetimerSecond)
-      openList = $(this).find('.second-category-list')
-      opentimerSecond = setTimeout(function(){
-        $('.second-category-list').not('hidden').addClass('hidden');
-        $(openList).removeClass('hidden');
-      }, 500)
+      var parentCategory = $(this).text();
+      $.ajax({
+        url: '/get_category_children',
+        type: 'get',
+        data: { parent_name: parentCategory },
+        dataType: 'json'
+      })
+      .done(function(children){
+        $('.second-category-list__item--link').remove();
+        children.forEach(function(child){
+          var html_child = `<a class="second-category-list__item--link" id="${child.id}" href="#">${child.name}</a>`
+          $('.second-category-list__item').append(html_child)
+        });
+        clearTimeout(closetimerSecond)
+        openList = $(document).find('.second-category-list')
+        opentimerSecond = setTimeout(function(){
+          $('.second-category-list').not('hidden').addClass('hidden');
+          $(openList).removeClass('hidden');
+      }, 100)
+      });
     },
-    'mouseout': function(){
-      clearTimeout(opentimerSecond);
-      var closeList = $(this).find('.second-category-list')
-      closetimerSecond = setTimeout(function(){
-        $(closeList).addClass('hidden');
-        if(openListThird) $(openListThird).addClass('hidden');
-      }, 400)
-    }
+    // 以下、最終調整のため残しておきます。 
+    // 'mouseout': function(){
+    //   clearTimeout(opentimerSecond);
+    //   var closeList = $(document).find('.second-category-list')
+    //   closetimerSecond = setTimeout(function(){
+    //     $(closeList).addClass('hidden');
+    //     if(openListThird) $(openListThird).addClass('hidden');
+    //   }, 100)
+    // }
   })
   var opentimerThird
   var closetimerThird
-  $('.second-category-list__item').on({
-    'mouseover': function(){
+  $(document).on("mouseover",".second-category-list__item--link", function(){
+      var childrenCategory = $(this).attr("id");
+      $.ajax({
+        url: '/get_category_grandchildren',
+        type: 'get',
+        data: { children_id: childrenCategory },
+        dataType: 'json'
+      })
+      .done(function(grandchildren){
+        $('.third-category-list__item--link').remove();
+        grandchildren.forEach(function(grandchild){
+          var html_grandchild = `<a class="third-category-list__item--link" id="grandchild_${grandchild.id}" href="#">${grandchild.name}</a>`
+          $('.third-category-list__item').append(html_grandchild)
+        });
       clearTimeout(closetimerThird)
-      openListThird = $(this).find('.third-category-list')
+      openListThird = $(document).find('.third-category-list')
       opentimerThird = setTimeout(function(){
         $('.third-category-list').not('hidden').addClass('hidden');
         $(openListThird).removeClass('hidden');
-      }, 500)
-    },
-    'mouseout': function(){
-      clearTimeout(opentimerThird);
-      var closeListThird = $(this).find('.third-category-list')
-      closetimerThird = setTimeout(function(){
-        $(closeListThird).addClass('hidden');
-      }, 400)
-    }
-  })
+      }, 100)
+      });
+    })
+  // 以下、最終調整のため残しておきます。
+  // $(document).on('mouseout','.second-category-list__item--link', function(){
+  //   clearTimeout(opentimerThird);
+  //   var closeListThird = $(document).find('.third-category-list')
+  //   closetimerThird = setTimeout(function(){
+  //     $(closeListThird).addClass('hidden');
+  //   }, 100)
+  // })
 })
--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -14,4 +14,14 @@ class GoodsController < ApplicationController
     @categories = Category.all.order("id ASC").limit(13)
   end
 
+  def get_category_children
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+    render json: @category_children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find(params[:children_id]).children
+    render json: @category_grandchildren
+  end
+
 end
