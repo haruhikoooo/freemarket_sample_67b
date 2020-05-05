49a77ea5790cb21e4bebee6819746d6c4a6070c9
haruhikoooo <haruhiko.ooka@gmail.com>
2020-05-05 17:23:04 +0900

    add user_id to goods table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_115829) do
+ActiveRecord::Schema.define(version: 2020_05_05_082011) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "destination_family_name", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_04_28_115829) do
     t.string "name", null: false
     t.integer "price", null: false
     t.integer "transaction_status_id", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
