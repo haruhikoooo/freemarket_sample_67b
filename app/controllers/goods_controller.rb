2707135e9a2914a8928245e5361c578c43799bf0
tetsuji1000 <kontetsu0206@gmail.com>
2020-05-06 11:24:36 +0900

    js　多数画像投稿

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -5,7 +5,25 @@ class GoodsController < ApplicationController
   end
 
   def new
+    @good = Good.new
+    @good.images.new
   end
+
+  def creat
+    @good = Good.new(good_params)
+    if @good.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def show
+  end
+  
+  private
+  
+  def good_params
+    params.require(:good).permit(name, images_attributes: [:image])
   end
 end
