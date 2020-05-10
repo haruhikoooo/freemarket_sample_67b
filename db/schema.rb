cb5db8cda9b19c595f6309279df25acc70e3daff
takacpa <t.ura0315@gmail.com>
2020-05-10 13:36:04 +0900

    set devise

--- db/schema.rb
+++ db/schema.rb
@@ -29,14 +29,6 @@ ActiveRecord::Schema.define(version: 2020_05_09_154353) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "ancestry"
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
-  end
-
   create_table "goods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
