31655c292b3f727f5b41e2ace4c07f3bbb2a2696
yossy0201 <yossy0201msk@gmail.com>
2020-05-11 22:33:20 +0900

    controller-edit

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -10,6 +10,10 @@ class GoodsController < ApplicationController
     @good = Good.find(params[:id])
   end
 
+  def edit
+    @good = Good.find(params[:id])
+  end
+
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
