1e9942e245561182e5f4305363862343a198fdf5
haruhikoooo <haruhiko.ooka@gmail.com>
2020-05-13 21:03:17 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 
   def show
+    user = User.find(params[:id])
+    unless current_user.id == user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 
 end
