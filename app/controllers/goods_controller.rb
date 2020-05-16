bd8324123275d5d19dff1fcf4fe9d619ce9dfcb8
yossy0201 <yossy0201msk@gmail.com>
2020-05-16 22:53:56 +0900

    delete

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,6 +1,5 @@
 class GoodsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  def index 
   before_action :authenticate_user!, only: [:new]
   before_action :category_index
   before_action :set_category, only: [:new, :edit, :create, :update, :destroy]
