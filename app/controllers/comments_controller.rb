00d810fe3a0a1615a4880332eba9ada117d4388b
haruhikoooo <haruhiko.ooka@gmail.com>
2020-06-04 21:34:30 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,15 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     comment = Comment.new(comment_params)
     @good = Good.find(params[:good_id])
-    redirect_to good_path(@good.id) and return
     if comment.save
       redirect_to good_path(@good.id)
     else
-      render "goods/#{@good.id}"
+      @comment = Comment.new
+      @comments = @good.comments.includes(:user)
+      render "goods/show"
     end
   end
   
