ca8e57b91b2017e673bc3ec087a402ff565a69cc
yossy0201 <yossy0201msk@gmail.com>
2020-06-05 21:16:18 +0900

    pay jp

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,26 +1,29 @@
-$(document).on('turbolinks:load', function() {
-  var form = $("#token_submit");
+$(function(){
+  var form = $("#charge-form");
   Payjp.setPublicKey('pk_test_708c14c6b540c53a94da7107');
-  $(document).on("click", "#token_submit", function(e) {
+  $("#charge-form").on("click", "#token_submit", function(e) {
+    
 
     e.preventDefault();
     form.find("input[type=submit]").prop("disabled", true);
 
     var card = {
-      card_number: $("#card_number").val(),
-        security_code: $("#security_code").val(),
-        expiration_data_month: $("#expiration_data_month").val(),
-        expiration_data_year: $("#expiration_data_year").val(),
+        number: $("#card_number").val(),
+        cvc: $("#security_code").val(),
+        exp_month: $("#expiration_data_month").val(),
+        exp_year: $("#expiration_data_year").val(),
     };
+    // console.log(card)
     Payjp.createToken(card, function(s, response) {
+      // console.log(s);
       if (response.error) {
         alert('トークン作成エラー発生');
       }
       else {
-        $(".card_number").removeAttr("name");
-        $(".security_code").removeAttr("name");
-        $(".expiration_data_month").removeAttr("name");
-        $(".expiration_data_year").removeAttr("name");
+        $(".number").removeAttr("name");
+        $(".cvc").removeAttr("name");
+        $(".exp_month").removeAttr("name");
+        $(".exp_year").removeAttr("name");
         var token = response.id;
         console.log(token)
         form.append($('<input type="hidden" name="payjpToken" />').val(token));
