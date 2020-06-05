381595dc3312a67a26dd1c2de30fb8e347efd3a6
yossy0201 <yossy0201msk@gmail.com>
2020-06-05 23:01:40 +0900

    pay jp

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -2,20 +2,15 @@ $(function(){
   var form = $("#charge-form");
   Payjp.setPublicKey('pk_test_708c14c6b540c53a94da7107');
   $("#charge-form").on("click", "#token_submit", function(e) {
-    
-
     e.preventDefault();
     form.find("input[type=submit]").prop("disabled", true);
-
     var card = {
         number: $("#card_number").val(),
         cvc: $("#security_code").val(),
         exp_month: $("#expiration_data_month").val(),
         exp_year: $("#expiration_data_year").val(),
     };
-    // console.log(card)
     Payjp.createToken(card, function(s, response) {
-      // console.log(s);
       if (response.error) {
         alert('トークン作成エラー発生');
       }
@@ -25,7 +20,6 @@ $(function(){
         $(".exp_month").removeAttr("name");
         $(".exp_year").removeAttr("name");
         var token = response.id;
-        console.log(token)
         form.append($('<input type="hidden" name="payjpToken" />').val(token));
         form.get(0).submit();
       }
