25a8f2768e203fbe450e6ada479656420f8413a7
yossy0201 <yossy0201msk@gmail.com>
2020-06-06 14:50:46 +0900

    payjp

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -8,11 +8,11 @@ class PaymentsController < ApplicationController
   def create #payjpとCardのデータベース作成
     Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
     #保管した顧客IDでpayjpから情報取得
-    if params["payjpToken"].blank?
+    if params['payjp-token'].blank?
       redirect_to new_user_payment_path
     else
       customer = Payjp::Customer.create(
-        card: params["payjpToken"],
+        payment: params['payjp-token'],
         metadata: {user_id: current_user.id}
       ) 
       @payment = Payment.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
@@ -36,15 +36,15 @@ class PaymentsController < ApplicationController
       redirect_to new_user_payment_path
   end
 
-  def index #paymentsのデータpayjpに送り情報を取り出す
+  def show #paymentsのデータpayjpに送り情報を取り出す
     @payment = Payment.find_by(user_id: current_user.id)
     if @payment.blank?
       redirect_to new_user_payment_path 
     else
       #payjpと通信するためのkeyをセットしました
       Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
-      customer = Payjp::Customer.retrieve(@payment.customer_id)
-      @default_card_information = customer.cards.retrieve(@payment.card_id)
+      customer = Payjp::Customer.retrieve(@payment.user_id)
+      @default_card_information = customer.cards.retrieve(@payment.card_number)
     end
   end
 end
